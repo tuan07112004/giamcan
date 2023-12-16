667caad6b432fd725dd18b490944112e38cf509c
tuan07112004 <dominhtuan002003@gmail.com>
2023-12-16 22:45:15 +0700

    okeee

---
+++ main.js
@@ -0,0 +1,13 @@
+function toggleForms() {
+    var loginSection = document.getElementById('user-login');
+    var registrationSection = document.getElementById('user-registration');
+  
+    if (loginSection.style.display === 'none') {
+      loginSection.style.display = 'block';
+      registrationSection.style.display = 'none';
+    } else {
+      loginSection.style.display = 'none';
+      registrationSection.style.display = 'block';
+    }
+  }
+  
